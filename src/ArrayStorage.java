69071fff5c54b6a95247cc0139c51aa85d2d1c55
TimaSerp <timaserp@gmail.com>
2022-03-31 20:08:05 +0300

    "выполнил первое дз по курсу basejava"

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,65 @@
 /**
  * Array based storage for Resumes
  */
+
+import java.util.Arrays;
+
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
+    private String uuid;
+    private int rIndexMax = 0;
 
     void clear() {
+        Arrays.fill(storage, 0, (rIndexMax), null);
+        rIndexMax = 0;
     }
 
     void save(Resume r) {
+        storage[rIndexMax] = r;
+        rIndexMax++;
     }
 
     Resume get(String uuid) {
+        if (checkExist()) {
+            for (int i = 0; i < rIndexMax; i++) {
+                if (uuid.equals(storage[i].getUuid())) {
+                    return storage[i];
+                }
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < rIndexMax; i++) {
+            if (uuid.equals(storage[i].getUuid())) {
+                storage[i].setUuid(storage[rIndexMax - 1].getUuid());
+                storage[rIndexMax - 1].setUuid(null);
+                rIndexMax--;
+                return;
+            }
+        }
+        rIndexMax--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] storageCopy = Arrays.copyOf(storage, rIndexMax);
+        return storageCopy;
     }
 
     int size() {
-        return 0;
+        return rIndexMax;
+    }
+
+    boolean checkExist() {
+        if (rIndexMax == 0) {
+            System.out.println("Error: there are no resume in your array");
+            return false;
+        } else {
+            return true;
+        }
     }
 }
