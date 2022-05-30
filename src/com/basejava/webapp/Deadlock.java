24be7870c9a295744d4719dbbb77ce18ea9f5600
TimaSerp <timaserp@gmail.com>
2022-05-30 19:42:23 +0300

    испарвил deadlock

--- src/com/basejava/webapp/Deadlock.java
+++ src/com/basejava/webapp/Deadlock.java
@@ -3,32 +3,27 @@ package com.basejava.webapp;
 public class Deadlock {
     private static final Object lock1 = new Object();
     private static final Object lock2 = new Object();
+    private static volatile int count;
 
     public static void main(String[] args) {
-        Thread thread1 = new Thread(() -> {
-            System.out.println("start " + Thread.currentThread().getName());
-            getDeadLock(lock1, lock2);
-            System.out.println("finish " + Thread.currentThread().getName());
-        }, "The first Thread");
-
-        Thread thread2 = new Thread(() -> {
-            System.out.println("start " + Thread.currentThread().getName());
-            getDeadLock(lock2, lock1);
-            System.out.println("finish " + Thread.currentThread().getName());
-        }, "The second Thread");
-
-        thread1.start();
-        thread2.start();
+        getDeadLock(lock1, lock2);
+        getDeadLock(lock2, lock1);
     }
 
     private static void getDeadLock(Object firstLock, Object secondLock) {
-        synchronized (firstLock) {
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        Thread thread = new Thread(() -> {
+            System.out.println("start " + Thread.currentThread().getName());
+            synchronized (firstLock) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (secondLock) {
+                }
             }
-            synchronized (secondLock) {}
-        }
+            System.out.println("finish " + Thread.currentThread().getName());
+        });
+        thread.start();
     }
 }
