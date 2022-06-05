e4c2a3199e01496d1a6b284b39f0fad1bef46ff3
TimaSerp <timaserp@gmail.com>
2022-06-06 00:34:12 +0300

    реализовал HW12 в один стрим

--- src/com/basejava/webapp/StreamApiUser.java
+++ src/com/basejava/webapp/StreamApiUser.java
@@ -3,14 +3,15 @@ package com.basejava.webapp;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamApiUser {
     public static void main(String[] args) {
-        int[] ints = new int[]{2, 1, 3, 3, 2, 3};
+        int[] ints = new int[]{2, 1, 8, 3, 2, 9};
         System.out.println(minValue(ints));
         List<Integer> integers = new ArrayList<>();
         integers.add(1);
-        integers.add(5);
+        integers.add(4);
         integers.add(7);
         integers.add(3);
         integers.add(8);
@@ -18,23 +19,16 @@ public class StreamApiUser {
     }
 
     private static int minValue(int[] values) {
-        int[] ints = Arrays.stream(values)
+        return Arrays.stream(values)
                 .distinct()
                 .sorted()
-                .toArray();
-        return Arrays.stream(ints)
-                .reduce(0, (identity, num) -> identity += num * (int) Math.pow(10, ints.length - Arrays.binarySearch(ints, num) - 1));
+                .reduce(0, (identity, num) -> identity += num * (int) Math.pow(10, Arrays.stream(values).distinct().count()
+                        - Arrays.binarySearch(Arrays.stream(values).distinct().sorted().toArray(), num) - 1));
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
-        int sum = (int) integers.stream()
-                .reduce(0, Integer::sum);
-        List<Integer> integersCopy = new ArrayList<>(integers);
-        int checkOddOrEven = sum % 2 == 0 ? 0 : 1;
-        integersCopy.stream()
-                .forEach(num -> {
-                    if (num % 2 == checkOddOrEven) integers.remove(num);
-                });
-        return integers;
+        return integers.stream()
+                .filter(num -> integers.stream().reduce(0, Integer::sum) % 2 == 1 ? num % 2 == 0 : num % 2 == 1)
+                .collect(Collectors.toList());
     }
 }
